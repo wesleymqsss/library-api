c25521914dc86c7a4fbfcc27de7c64cb69b1833d
Wesley Marques <wesleymqsss@gmail.com>
2025-06-08 12:44:39 -0300

    modification in the model - new attributes were  added and data automation through Auditin Entity

--- src/main/java/io/github/cursospring/libraryapi/LibraryapiApplication.java
+++ src/main/java/io/github/cursospring/libraryapi/LibraryapiApplication.java
@@ -2,8 +2,10 @@ package io.github.cursospring.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // necessario para modificar as datas de UPDATE E CREAT
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/github/cursospring/libraryapi/model/Autor.java
+++ src/main/java/io/github/cursospring/libraryapi/model/Autor.java
@@ -3,8 +3,12 @@ package io.github.cursospring.libraryapi.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,13 +16,16 @@ import java.util.UUID;
 @Table(name = "autor", schema = "public")
 @Data
 @ToString
+@EntityListeners(AuditingEntityListener.class) // necessaria para fazer o preenchimento automatico das datas de UPDATE e E CREAT
+//TAMBÉM É NECESSARIO UMA OUTRA ANOTACAO NA CLASS APPLICATION PARA O FUNCIONAMENTO
 public class Autor {
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "name", length = 100, nullable = false)
+    @Column(name = "nome", length = 100, nullable = false)
     private String nome;
 
     @Column(name = "data_nascimento" , nullable = false)
@@ -28,8 +35,44 @@ public class Autor {
     private String nacionalidade;
 
 //    @OneToMany(mappedBy = "autor")
-    @Transient
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Livro> livros;
+
+    @CreatedDate //sempre que for CRIADO um novo usuario a data CADASTRO é preenchida de forma autonoma
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate //sempre que for ATUALIZADO um novo usuario a data ATUALIZACAO é preenchida de forma autonoma
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
+
+    //getter and setter
+    public LocalDateTime getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public UUID getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(UUID idUsuario) {
+        this.idUsuario = idUsuario;
+    }
 
     public LocalDate getDataNascimento() {
         return dataNascimento;
